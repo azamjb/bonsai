045723ccc16a9f3ca2ba7cd5154bb94472effb0b
azamjb <azamjb123@gmail.com>
2025-02-06 00:46:25 -0500

    Education page

--- bonsai/Modules/EducateModule/EducateView.swift
+++ bonsai/Modules/EducateModule/EducateView.swift
@@ -7,36 +7,138 @@
 
 import SwiftUI
 
-
 struct EducateView: View {
     @StateObject var viewModel: EducateViewModel = EducateViewModel()
     @Environment(\.dismiss) private var dismiss // Environment property for dismissing the view
 
-    
     var body: some View {
-        NavigationStack{
-            VStack {
-                Text("Educate Page")
-                    .font(.title)
-                    .padding(.bottom, 20)
-                
-                NavigationLink(destination: ProfileCreation1View()) {
-                    Text("I'm Inspired. Scary phone stuff!!")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.black)
-                        .cornerRadius(12)
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    
+                    Text("Welcome To")
+                        .font(.headline)
+                        .font(.system(size: 20, weight: .bold))
+                        .padding(.top, 60)
+
+                    Text("BONSAI.")
+                        .font(.system(size: 240, weight: .black))
+                        .italic()
+                        .lineSpacing(0)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 160)
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text("Find Your Balance")
+                            .font(.title2)
+                            .padding(.bottom, 5)
+                        
+                        Text("• Choose the apps you want to track")
+                            .font(.caption)
+                        
+                        Text("• Set daily screen time limits")
+                            .font(.caption)
+                        
+                        Text("• Take control of your time")
+                            .font(.caption)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 5)
+                    .padding(.bottom, 160)
+                    
+                    
+                    
+                    VStack(alignment: .trailing) {
+                        
+                        Text("Partner with a Purpose")
+                            .font(.title2)
+                            .padding(.bottom, 5)
+                        
+                        Text("• Choose your accountability partner")
+                            .font(.caption)
+                        
+                        Text("• your partner can grant you extra time")
+                            .font(.caption)
+                        
+                        Text("• Don't want to reach out? be prepared to pay")
+                            .font(.caption)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.trailing, 5)
+                    .padding(.bottom, 160)
+                    
+                    
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text("Own Your Time")
+                            .font(.title2)
+                            .padding(.bottom, 5)
+                        
+                        Text("• Apps will be blocked once you hit your time limit")
+                            .font(.caption)
+                        
+                        Text("• Accountability partners help you stick to your limits.")
+                            .font(.caption)
+                        
+                        Text("• Use your time wisely and productively")
+                            .font(.caption)
+                        
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 5)
+                    .padding(.bottom, 160)
+                    
+                    
+                    
+                    VStack(alignment: .trailing) {
+                        
+                        Text("Track. Grow. Balance.")
+                            .font(.title2)
+                            .padding(.bottom, 5)
+                        
+                        Text("• View detailed analytics of your screen time")
+                            .font(.caption)
+                        
+                        Text("• Monitor your progress toward your goals")
+                            .font(.caption)
+                        
+                        Text("• Achieve balance in your digital life")
+                            .font(.caption)
+                        
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.trailing, 5)
+                    .padding(.bottom, 160)
+
+                    Image("tagline")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 230)
+                        .padding(.bottom,100)
+                        
+                    
+                    NavigationLink(destination: ProfileCreation1View()) {
+                        HStack {
+                            Text("Get Started")
+                                .font(.system(size: 18))
+                                .foregroundStyle(.black)
+
+                            Image(systemName: "arrowshape.right.fill")
+                                .font(.system(size: 20))
+                                .foregroundStyle(.black)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.trailing, 40)
                 }
-                
-                .foregroundColor(.white)
                 .padding()
-                .frame(maxWidth: .infinity)
-                .background(Color.black)
-                .cornerRadius(12)
             }
-            .padding()
+            .scrollIndicators(.hidden)
             .preferredColorScheme(.light)
+            .navigationBarHidden(true)
         }
     }
 }
